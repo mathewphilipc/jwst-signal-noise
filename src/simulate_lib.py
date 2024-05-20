b3724ee2a5bdc23d65ab40b9e650ba01c5400de8
Mathew Calkins <mathewpcalkins@gmail.com>
2024-05-20 17:16:00 -0400

    Simulate many multiaccum processes, compute statistics of distribution of fitted slopes

--- src/simulate_lib.py
+++ src/simulate_lib.py
@@ -2,10 +2,11 @@ import argparse
 import random
 import numpy as np
 
-def simulate_multiaccum(freq, num_measurements, read_noise=0):
+def simulate_multiaccum(freq, num_measurements, read_noise):
     """
     Simulates the data output of a MULTIACCUM process. We freely pick time units so the
-    experiment has unit duration. In the notation of arxiv.org:0706.2344 we have:
+    experiment has unit duration. We also have one frame per group (no frame averaging)
+    and treat a single pixel. In the notation of arxiv.org:0706.2344 we have:
         (arXiv notation) = (local notation)
         f = freq
         sigma_read = read_noise
@@ -13,7 +14,6 @@ def simulate_multiaccum(freq, num_measurements, read_noise=0):
         n = num_measurements
         tf = 1/(num_measurements - 1) (dt below)
         tg = tf (no frame average)
-
 
     Parameters:
     num_measurements (int): Number of experimental measurements taken.
@@ -32,7 +32,35 @@ def simulate_multiaccum(freq, num_measurements, read_noise=0):
     exp_output = [round(x + np.random.normal(0, read_noise)) for x in exp_output]
     return exp_output
 
-def multiaccum_variance(read_noise, freq, groups, frames_per_group, frame_time, group_time):
+def empirical_ols_multiaccum_statistics(read_noise, freq, num_measurements, num_trials):
+    """
+    Simulates many multiaccum processes, calculates fitted freq for each, and return sample mean + stddev.
+    As above we treat a single pixel sans frame averaging and measurements start at t=0.0, end at 1.0.
+    With enough trials this should agree with theoretical_ols_multiaccum_variance at
+    frames_per_group=1, groups=num_measurements, frame_time=group_time=1.0/(num_measurements - 1).
+
+    Parameters:
+    num_measurements (int): Number of experimental measurements taken.
+    freq (float): Expected frequency of events per experiment.
+    read_noise (float): Standard deviation of read noise (IID Gaussian).
+
+    Returns:
+    array[float]: [mean, stddev] of all fitted slopes.
+
+    """
+    dt = 1.0 / (num_measurements - 1)
+    fitted_slopes = []
+    for sample in range(num_samples):
+        exp_output = simulate_multiaccum(freq, num_measurements, read_noise)
+        time_array = np.array([i*dt for i in range(num_measurements)])
+        fitted_coeffs = np.polyfit(time_array, np.array(exp_output), 1)
+        fitted_slopes.append(fitted_coeffs[0])
+
+    return [np.mean(fitted_slopes), np.std(fitted_slope)]
+
+
+
+def theoretical_ols_multiaccum_stddev(read_noise, freq, groups, frames_per_group, frame_time, group_time):
     """
     Predicts the sample stddev associated with running a MULTIACCUM process with
     exactly known parameters, then fitting by OLS to estimate the true freq value.
@@ -59,5 +87,5 @@ def multiaccum_variance(read_noise, freq, groups, frames_per_group, frame_time, 
     second_term = 6*(n**2 + 1)*(n - 1)*tg*freq/(5*n*(n + 1))
     third_term = -2*(2*m - 1)*(n - 1)*(m - 1)*tf*freq / (m*n*(n+1))
 
-    return first_term + second_term + third_term
+    return np.sqrt(first_term + second_term + third_term)
 
