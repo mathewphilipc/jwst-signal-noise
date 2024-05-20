6d1aea0c038ef3ca0b240319df82164100f5a1f1
Mathew Calkins <mathewpcalkins@gmail.com>
2024-05-20 16:46:43 -0400

    multiaccum simulations should start at 0.0, not dt

--- src/simulate_lib.py
+++ src/simulate_lib.py
@@ -22,11 +22,11 @@ def simulate_multiaccum(freq, num_measurements, read_noise=0):
 
     Returns:
     list: A list containing the cumulative event count at each measurement.
-    Initial entry is the start of Poisson process, but still has read noise.
+    Initial entry is t = 0, final is t = 1.0.
     """
 
     dt = 1.0 / (num_measurements - 1)
-    exp_output = [np.random.poisson(freq*dt)]
+    exp_output = [0]
     for i in range(num_measurements - 1):
         exp_output.append(exp_output[i] + np.random.poisson(freq*dt))
     exp_output = [round(x + np.random.normal(0, read_noise)) for x in exp_output]
