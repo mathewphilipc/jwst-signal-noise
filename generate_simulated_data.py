34a3aef7ae4fb5383a940db4e4cbce3726f517cd
Mathew <mathewpcalkins@gmail.com>
2025-04-10 15:00:54 -0400

    more simulations

--- generate_simulated_data.py
+++ generate_simulated_data.py
@@ -3,14 +3,15 @@ import pandas as pd
 print("hello")
 
 freq = 100.0
-num_measurements = 20
+num_measurements = 100
 read_noise = 0.0
+num_samples = 10000
 
 #monte_carlo_data = simulate_multiaccum(freq=100.0, num_measurements=100, read_noise = 0.0)
 #print(monte_carlo_data)
 
 data =[]
-for i in range(10):
+for i in range(num_samples):
     curr_data = simulate_multiaccum(freq=freq, num_measurements=num_measurements, read_noise=read_noise)
     data.append(curr_data)
 
