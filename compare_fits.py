34d517d6c64ac4493dd74aed070f3a24bf12b9e9
Mathew <mathewpcalkins@gmail.com>
2025-04-17 14:48:11 -0400

    clean up

--- compare_fits.py
+++ compare_fits.py
@@ -2,49 +2,52 @@ import pandas as pd
 import numpy as np
 from fitramp.fitramp import Covar, Ramp_Result, fit_ramps
 import matplotlib.pyplot as plt
+import math
 import statistics
 
-df = pd.read_csv("data/poisson_simulations/freq_100_read_noise_0/simulations.csv", index_col=0)
-print(df)
-print("num columns:")
-print(len(df.columns))
-num_exps = len(df.columns)
-ols_slopes = []
-ols_intercepts = []
-brandt_slopes = []
 
-for i in range(num_exps):
-    curr_data = df.iloc[:, i].tolist()
-    #print("\nCurrent data:")
-    #print(curr_data)
-    if (i%100==0):
-        print("Studying sample", i)
+def fit_naive_slope(data):
+    # Estimate slope naively
+    num_measurements = len(data)
+    diff = data[num_measurements - 1] - data[0]
+    return diff / (num_measurements - 1)
 
-    #print("Estimating from OLS...")
+def fit_ols_slope(data):
+    # Estimate slope by OLS
     n = len(curr_data)
     X = np.vstack([np.linspace(1, n, n), np.ones(n)]).T
     ols_fit_params = np.linalg.lstsq(X, curr_data, rcond=None)[0] # (2,) nparr containing [slope, intercept]
-    #print(f"OLS-estimated countrate slope = {ols_fit_params[0]}")
     ols_slopes.append(float(ols_fit_params[0]))
-    #print(f"OLS-estimated countrate intercept = {ols_fit_params[1]}")
-    ols_intercepts.append(float(ols_fit_params[1]))
+    return float(ols_fit_params[1])
 
-    #print("Estimating Brandt-style...")
+def fit_brandt_slope(data):
+    # Estimate slope by Brandt fitramp
+    n = len(curr_data)
     my_covar = Covar([s for s in range(n)])
     diffs = np.ndarray(shape=(n-1,1), dtype=np.int64)
     for t in range(1,len(diffs)+1):
         diffs[t-1] = curr_data[t] - curr_data[t-1]
     ramp_result = fit_ramps(diffs = diffs, Cov = my_covar, sig=20.1, rescale=False)
     brandt_fit_slope = ramp_result.countrate[0]
-    #print(f"Brandt estimated count rate = {brandt_fit_slope}")
-    brandt_slopes.append(brandt_fit_slope)
+    return brandt_fit_slope
 
-#print("\n\n\n\nOLS slopes:")
-#print(ols_slopes)
-#print("OLS intercepts:")
-#print(ols_intercepts)
+df = pd.read_csv("data/poisson_simulations/freq_100_read_noise_0/simulations.csv", index_col=0)
+num_exps = len(df.columns)
+ols_slopes = []
+ols_intercepts = []
+brandt_slopes = []
+naive_slopes = []
 
-num_bins = 20
+for i in range(num_exps):
+    curr_data = df.iloc[:, i].tolist()
+    if (i%100==0):
+        print("Studying sample", i)
+    ols_intercepts.append(fit_ols_slope(curr_data))
+    brandt_slopes.append(fit_brandt_slope(curr_data))
+    naive_slopes.append(fit_naive_slope(curr_data))
+
+
+num_bins = 40
 
 plt.hist(ols_slopes, bins=num_bins)
 plt.xlabel('OLS-estimated slope')
@@ -58,11 +61,36 @@ plt.ylabel('Frequency')
 plt.title('Distribution of Brandt-estimated slopes sans read noise')
 plt.show()
 
-print("OLS summary:")
-print("mean:", statistics.mean(ols_slopes))
-print("stdev:", statistics.stdev(ols_slopes))
+plt.hist(naive_slopes, bins=num_bins)
+plt.xlabel('Naive-estimated slope')
+plt.ylabel('Frequency')
+plt.title('Distribution of naive-estimated slopes sans read noise')
+plt.show()
 
-print("Brandt summary:")
-print("mean:", statistics.mean(brandt_slopes))
-print("stdev:", statistics.stdev(brandt_slopes))
 
+print("\nOLS summary:")
+mean = statistics.mean(ols_slopes)
+stdev = statistics.stdev(ols_slopes)
+stderr = stdev / math.sqrt(len(ols_slopes) - 1)
+print(f"mean = {mean}")
+print(f"stdev = {stdev}")
+print(f"stderr = {stderr}")
+print(f"2-sigma confidence interval = [{mean - 2*stderr}, {mean + 2*stderr}]")
+
+print("\nBrandt summary:")
+mean = statistics.mean(brandt_slopes)
+stdev = statistics.stdev(brandt_slopes)
+stderr = stdev / math.sqrt(len(brandt_slopes) - 1)
+print(f"mean = {mean}")
+print(f"stdev = {stdev}")
+print(f"stderr = {stderr}")
+print(f"2-sigma confidence interval = [{mean - 2*stderr}, {mean + 2*stderr}]")
+
+print("\nNaive summary:")
+mean = statistics.mean(naive_slopes)
+stdev = statistics.stdev(naive_slopes)
+stderr = stdev / math.sqrt(len(naive_slopes) - 1)
+print(f"mean = {mean}")
+print(f"stdev = {stdev}")
+print(f"stderr = {stderr}")
+print(f"2-sigma confidence interval = [{mean - 2*stderr}, {mean + 2*stderr}]")
