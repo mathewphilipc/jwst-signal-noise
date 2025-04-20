fb434248ae1285b88a4d0e65270a0406bb9f3c3e
Mathew Calkins <mathewpcalkins@gmail.com>
2025-04-20 13:21:32 -0400

    clean up + pass freq/read_noise as flags

--- generate_simulated_data.py
+++ generate_simulated_data.py
@@ -1,18 +1,29 @@
 from src.simulate_lib import simulate_multiaccum
 import pandas as pd
-print("hello")
+import argparse
 
-freq = 10000 # Mathematically makes sense to take floats, but suffices for our tests to restrict to integers
+parser.add_argument("--truefreq", 
+                    type=float, 
+                    required=True, 
+                    help="True (simulated) frequency"))
+
+parser.add_argument("--readnoise",
+                    type=int, 
+                    required=True, 
+                    help="Standard deviation of simulated read noise"
+)
+
+args = parser.parse_args()
+
+true_freq = args.truefreq
+read_noise = args.readnoise
+
 num_measurements = 101 # Includes measurements at exactly t=0.0 and t=1.0.
-read_noise = 1000
 num_samples = 10000 # Generally 10,000
-
-#monte_carlo_data = simulate_multiaccum(freq=100.0, num_measurements=100, read_noise = 0.0)
-#print(monte_carlo_data)
 
 data =[]
 for i in range(num_samples):
-    curr_data = simulate_multiaccum(freq=freq, num_measurements=num_measurements, read_noise=read_noise)
+    curr_data = simulate_multiaccum(freq=true_freq, num_measurements=num_measurements, read_noise=read_noise)
     data.append(curr_data)
 
 df = pd.DataFrame(data).T
