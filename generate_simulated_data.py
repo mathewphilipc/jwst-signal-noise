cae275470eb4da34d9a7e6b9733d55925ad18069
Mathew Calkins <mathewpcalkins@gmail.com>
2025-04-20 13:40:13 -0400

    bug fixes

--- generate_simulated_data.py
+++ generate_simulated_data.py
@@ -2,10 +2,13 @@ from src.simulate_lib import simulate_multiaccum
 import pandas as pd
 import argparse
 
+parser = argparse.ArgumentParser()
+
 parser.add_argument("--truefreq", 
-                    type=float, 
+                    type=int, 
                     required=True, 
-                    help="True (simulated) frequency"))
+                    help="True (simulated) frequency"
+)
 
 parser.add_argument("--readnoise",
                     type=int, 
@@ -15,7 +18,7 @@ parser.add_argument("--readnoise",
 
 args = parser.parse_args()
 
-true_freq = args.truefreq
+freq = args.truefreq
 read_noise = args.readnoise
 
 num_measurements = 101 # Includes measurements at exactly t=0.0 and t=1.0.
@@ -23,7 +26,7 @@ num_samples = 10000 # Generally 10,000
 
 data =[]
 for i in range(num_samples):
-    curr_data = simulate_multiaccum(freq=true_freq, num_measurements=num_measurements, read_noise=read_noise)
+    curr_data = simulate_multiaccum(freq=freq, num_measurements=num_measurements, read_noise=read_noise)
     data.append(curr_data)
 
 df = pd.DataFrame(data).T
