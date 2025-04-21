cb385c1a919fa14ac85f10e4bbb5d1a7985d04f7
Mathew Calkins <mathewpcalkins@gmail.com>
2025-04-21 17:26:05 -0400

    wider ranger of freqs and noises

--- compare_many_fits.sh
+++ compare_many_fits.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-for truefreq in 1 10; do
-  for readnoise in 1 10; do
+for truefreq in 1 10 100 1000 10000; do
+  for readnoise in 1 10 100 1000 10000; do
     python3 compare_fits.py --truefreq "$truefreq" --readnoise "$readnoise"
   done
 done
