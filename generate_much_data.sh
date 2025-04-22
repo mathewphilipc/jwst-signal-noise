25a764e517bab0b676beab0133e5c34d81d5517f
Mathew Calkins <mathewpcalkins@gmail.com>
2025-04-22 13:14:05 -0400

    finer freq/noise resolution in scripts

--- generate_much_data.sh
+++ generate_much_data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-for truefreq in 1 30 10 300 100 1000 10000; do
-  for readnoise in 1 30 10 300 100 1000 10000; do
+for truefreq in 1 3 10 30 100 300 1000 10000; do
+  for readnoise in 1 3 10 30 100 300 1000 10000; do
     python3 generate_simulated_data.py --truefreq "$truefreq" --readnoise "$readnoise"
   done
 done
