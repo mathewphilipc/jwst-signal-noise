624b62e1a14ad51070e762606afd6c8322bb557e
Mathew Calkins <mathewpcalkins@gmail.com>
2025-05-12 17:51:26 -0400

    compare wider range of fits

--- compare_many_fits.sh
+++ compare_many_fits.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-for truefreq in 1 3 10 30 100 300 1000 10000; do
-  for readnoise in 1 3 10 30 100 300 1000 10000; do
+for truefreq in 1 3 10 30 100 300 1000; do
+  for readnoise in 1 3 10 30 100 300 1000; do
     python3 compare_fits.py --truefreq "$truefreq" --readnoise "$readnoise"
   done
 done
