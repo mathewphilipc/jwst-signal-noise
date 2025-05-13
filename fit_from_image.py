35f1574a12dead9d0dbadafc92b656c76f54d860
Mathew Calkins <mathewpcalkins@gmail.com>
2025-05-13 13:57:06 -0400

    fit from image should compute chisq, not ssre

--- fit_from_image.py
+++ fit_from_image.py
@@ -3,6 +3,31 @@ from astropy.io import fits
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+from src.simulate_lib import poisson_chisq
+
+# Estimate slope by OLS
+def fit_ols_slope_and_chisq(data):
+    n = len(data)
+    X = np.vstack([np.linspace(1, n, n), np.ones(n)]).T
+    ols_fit_params = np.linalg.lstsq(X, data, rcond=None)[0] # (2,) nparr containing [slope, intercept]
+    slope = float(ols_fit_params[0])
+    intercept = float(ols_fit_params[1])
+    chisq = poisson_chisq(history=data, intercept=intercept, slope=slope, read_noise=read_noise)
+    return [slope, chisq]
+
+# Estimate slope naively
+def fit_brandt_slope_and_chisq(data):
+    n = len(data)
+    my_covar = Covar([s for s in range(n)])
+    diffs = np.ndarray(shape=(n-1,1), dtype=np.int64)
+    for t in range(1,len(diffs)+1):
+        diffs[t-1] = data[t] - data[t-1]
+    #sig = 20.1 for JWST images
+    ramp_result = fit_ramps(diffs = diffs, Cov = my_covar, sig=float(read_noise), rescale=True)
+    slope = ramp_result.countrate[0]
+    #intercept = ramp_result.pedestal[0]
+    chisq = poisson_chisq(history=data, intercept=0, slope=slope, read_noise=read_noise)
+    return [slope, chisq]
 
 read_noise = 12.3
 
@@ -41,63 +66,40 @@ print(f"New history shape = {image_history.shape}")
 naive_reconstruction = np.zeros((y_len, x_len))
 brandt_reconstruction = np.zeros((y_len, x_len))
 
-ols_ssre_list = []
-brandt_ssre_list = []
 ols_slope_list = []
+brandt_chisq_list = []
+ols_chisq_list = []
 
 for i in range(x_len):
     for j in range(y_len):
         X = np.vstack([np.linspace(1, n, n), np.ones(n)]).T
 
         pixel_history = image_history[:, y_min + j, x_min + i]
-        ols_fit_params = np.linalg.lstsq(X, pixel_history, rcond=None)[0] # (2,) nparr containing [slope, intercept]
-        naive_reconstruction[j][i] = ols_fit_params[0]
-        print(f"\nOLS estimated count rate slope = {ols_fit_params[0]}")
-        ols_slope_list.append(ols_fit_params[0])
+        [ols_slope, ols_chisq] = fit_ols_slope_and_chisq(pixel_history)
+        ols_slope_list.append(ols_slope)
+        ols_chisq_list.append(ols_chisq)
+        naive_reconstruction[j][i] = ols_slope
 
-        # Computed sum of squared relative errors for OLS
-        predicted_values = X @ ols_fit_params
-        residuals = pixel_history - predicted_values
-        ssre_statistic = math.sqrt(np.sum((residuals / pixel_history)**2))
-        print(f"OLS ssre = {ssre_statistic}")
-        ols_ssre_list.append(ssre_statistic)
+        [brandt_slope, brandt_chisq] = fit_ols_slope_and_chisq(pixel_history)
+        brandt_chisq_list.append(brandt_chisq)
+        brandt_reconstruction[j][i] = brandt_slope
 
-        fit_intercept = True
-        my_covar = Covar(read_times=[s + 1 for s in range(n)], pedestal=fit_intercept)
-        diffs = np.ndarray(shape=(n-1,1), dtype=np.int64)
-        for t in range(1,len(diffs)+1):
-            diffs[t-1] = pixel_history[t] - pixel_history[t-1]
-        if (fit_intercept):
-            diffs = np.insert(diffs, 0, pixel_history[0], axis=0)
-        ramp_result = fit_ramps(diffs = diffs, Cov = my_covar, sig=read_noise, rescale=True)
-        brandt_fit_slope = ramp_result.countrate[0]
-        print(f"Brandt estimated count rate = {brandt_fit_slope}")
-        brandt_fit_intercept = ramp_result.pedestal[0]
-        brandt_reconstruction[j][i] = brandt_fit_slope
-
-        # Computed sum of squared relative errors for Brandt
-        predicted_values = X @ np.array([brandt_fit_slope, brandt_fit_intercept])
-        residuals = pixel_history - predicted_values
-        ssre_statistic = math.sqrt(np.sum((residuals / pixel_history)**2))
-        print(f"Brandt ssre = {ssre_statistic}")
-        brandt_ssre_list.append(ssre_statistic)
-
-print(f"\n\n\nMean OLS ssre = {np.median(ols_ssre_list)}")
-print(f"\n\n\nMean brandt bootstrap ssre = {np.median(brandt_ssre_list)}")
-ssre_ratios = [(brandt_ssre_list[i] / ols_ssre_list[i]) for i in range(len(ols_ssre_list))]
-print(f"\n\n\nList of ssre ratios:")
-print(f"\n\n\nmedian ssre_ratio = {np.median(ssre_ratios)}")
-print(f"\n\n\nmean ssre_ratio = {np.mean(ssre_ratios)}")
-print(f"\n\n\nmin sqrt_ratio = {np.min(ssre_ratios)}")
-print(f"\n\n\nSorted list of ratios: {sorted(ssre_ratios)}")
+print(f"\n\n\nMean OLS chisq = {np.median(ols_chisq_list)}")
+print(f"\n\n\nMean brandt bootstrap chisq = {np.median(brandt_chisq_list)}")
+chisq_ratios = [(brandt_chisq_list[i] / ols_chisq_list[i]) for i in range(len(ols_chisq_list))]
+print(f"\n\n\nList of chisq ratios:")
+print(f"\n\n\nmedian chisq_ratio = {np.median(chisq_ratios)}")
+print(f"\n\n\nmean chisq_ratio = {np.mean(chisq_ratios)}")
+print(f"\n\n\nmin sqrt_ratio = {np.min(chisq_ratios)}")
+print(f"\n\n\nSorted list of ratios: {sorted(chisq_ratios)}")
 
 
-plt.hist(ssre_ratios, bins=100)
-plt.title('Frequency histogram of Brandt:OLS sum squared relative error ratio')
+plt.hist(schisq_ratios, bins=100)
+plt.title('Frequency histogram of Brandt:OLS chisq ratio')
 plt.ylabel('Frequency')
-plt.xlabel('SSRE ratio')
+plt.xlabel('chisq ratio')
 plt.savefig(
-    "data/plots/ssre_ratio_frequency.png",
+    "data/plots/chisq_ratio_frequency.png",
     dpi=300,
     bbox_inches="tight"
 )
@@ -105,13 +107,13 @@ plt.show()
 
 
 x = ols_slope_list
-y = ssre_ratios
+y = chisq_ratios
 plt.scatter(x,y)
 plt.xlabel('OLS-estimated rate')
-plt.ylabel('Ratio of SSREs')
-plt.title('Brandt:OLS sum squared relative error ratio vs OLS-estimated rate')
+plt.ylabel('Ratio of chisqs')
+plt.title('Brandt:OLS chisq ratio vs OLS-estimated rate')
 plt.savefig(
-    "data/plots/ssre_ratio_versus_rate.png",
+    "data/plots/chisq_ratio_versus_rate.png",
     dpi=300,
     bbox_inches="tight"
 )
